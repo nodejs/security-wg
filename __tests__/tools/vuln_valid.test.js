c6ba39227657490135d34ba4600b93c3e71dd6c8
Vibhav Bobade <vibhav.bobde@gmail.com>
2019-01-03 23:31:10 +0530

    Added tests for vuln_valid

---
+++ __tests__/tools/vuln_valid.test.js
@@ -0,0 +1,28 @@
+const vulnValid = require('../../tools/vuln_valid')
+
+const vulnPathsPass = require('../../__mocks__/mockVuln/pass').paths
+const vulnPathsFail = require('../../__mocks__/mockVuln/fail').paths
+const joi = require('joi')
+
+describe("Testing vuln_valid", () => {
+    test("Testing validate for core", async ()=>{
+        expect(() => 
+            vulnValid.validateVuln(
+            vulnPathsPass.core+"/1.json",vulnValid.coreModel)
+        ).not.toThrow()
+        expect(() => 
+            vulnValid.validateVuln(
+            vulnPathsFail.core+"/1.json",vulnValid.coreModel)
+        ).toThrow()
+    })
+    test("Testing validate for npm", async ()=>{
+        expect(() => 
+            vulnValid.validateVuln(
+            vulnPathsPass.npm+"/1.json",vulnValid.npmModel)
+        ).not.toThrow()
+        expect(() => 
+            vulnValid.validateVuln(
+            vulnPathsFail.core+"/1.json",vulnValid.npmModel)
+        ).toThrow()
+    })
+})
