5b45e38eaa9a1bb9532e2fbeb0566d4821c10e7c
Vibhav Bobade <vibhav.bobde@gmail.com>
2019-01-30 19:17:52 +0530

    Fixed formatting

--- tools/vuln_valid/index.js
+++ tools/vuln_valid/index.js
@@ -1,7 +1,7 @@
-'use strict'
+"use strict";
 
-const vulnValidate = require('./vulnValidate');
-const vulnPaths = require('../../vuln').paths;
+const vulnValidate = require("./vulnValidate");
+const vulnPaths = require("../../vuln").paths;
 
 vulnValidate.validate(vulnPaths.core, vulnValidate.coreModel);
 vulnValidate.validate(vulnPaths.npm, vulnValidate.npmModel);
