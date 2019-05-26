a2620ad6128cb8cb1798bd35463d593d981c6dbc
vdeturckheim <vlad2t@hotmail.com>
2019-05-26 19:04:53 +0300

    remove vulnerabilities from this repo

--- tools/vuln_valid/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const vulnValidate = require("./vulnValidate");
-const vulnPaths = require("../../vuln").paths;
-
-vulnValidate.validate(vulnPaths.core, vulnValidate.coreModel);
-vulnValidate.validate(vulnPaths.npm, vulnValidate.npmModel);
