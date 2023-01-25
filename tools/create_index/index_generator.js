0c3a5971af2eff23f33ab59b1018ca4fe7d1ca8b
Rafael Gonzaga <rafael.nunu@hotmail.com>
2023-01-25 21:03:37 +0000

    vuln: update core generator to filter active release lines (#866)

--- tools/create_index/index_generator.js
+++ tools/create_index/index_generator.js
@@ -1,7 +1,8 @@
-var fs = require('fs')
+const fs = require('node:fs')
+const satisfies = require('semver/functions/satisfies')
 
 // We will fill in this object with our data
-var vuln = {}
+let vuln = {}
 
 const createIndex = function (vulnDirectoryPath) {
   const files = fs.readdirSync(vulnDirectoryPath)
@@ -9,23 +10,37 @@ const createIndex = function (vulnDirectoryPath) {
   writeIndex(vuln, vulnDirectoryPath)
 }
 
+const supportedVersions = ['14.0.0', '16.0.0', '18.0.0', '19.0.0']
+
+const supportedVersionAffected = function (json) {
+  for (const version of supportedVersions) {
+    if (satisfies(version, json.vulnerable)) {
+      return true
+    }
+  }
+  return false
+}
+
 const getVulnDirectoryContents = function (files, vulnDirectoryPath) {
   for(file of files) {
     const filename = file.slice(-0, file.toString().indexOf('.json'))
     if(filename !== 'index') {
       const data = fs.readFileSync(vulnDirectoryPath + file)
-      createVulnObject(filename, data)
+      const json = JSON.parse(data)
+      if (supportedVersionAffected(json)) {
+        createVulnObject(filename, json)
+      }
     }
   }
 }
 
 const createVulnObject = function(identifier, json) {
-  vuln[identifier] = JSON.parse(json)
+  vuln[identifier] = json
 }
 
 const writeIndex = function(data, vulnDirectoryPath) {
   fs.writeFileSync(vulnDirectoryPath + 'index.json', JSON.stringify(data))
-  
+
   if(vulnDirectoryPath === './vuln/core/') {
     console.log('Succesfully wrote ' + vulnDirectoryPath + 'index.json for core vulnerabilities.')
   } else if(vulnDirectoryPath === './vuln/npm/') {
