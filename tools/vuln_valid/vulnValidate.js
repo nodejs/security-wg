f03c35948920322f92260638830918f57679027c
Marco Ippolito <marcoippolito54@gmail.com>
2026-01-27 13:41:02 +0100

    feat: add vex generator (#1537)
    * feat: add vex generator
    
    * chore: bump node versions
    
    * fix: update validator function

--- tools/vuln_valid/vulnValidate.js
+++ tools/vuln_valid/vulnValidate.js
@@ -25,8 +25,7 @@ const coreModel = Joi.object({
   publish_date: Joi
     .string()
     .regex(/^\d{4}-\d{2}-\d{2}$/)
-    .optional()
-    .isoDate(),
+    .optional(),
   type: Joi.string().optional(),
   cvss_score: Joi.number().optional(),
   cvss: Joi.string().optional(),
@@ -43,9 +42,32 @@ const coreModel = Joi.object({
     .required()
 });
 
+const npmModel = Joi.object({
+  id: Joi.number().required(),
+  cves: Joi.array().items(Joi.string().regex(/CVE-\d{4}-\d+/)).required(),
+  created_at: Joi.string().regex(/^\d{4}-\d{2}-\d{2}$/).required(),
+  updated_at: Joi.string().regex(/^\d{4}-\d{2}-\d{2}$/).required(),
+  title: Joi.string().max(150).regex(/^[^\n]+$/).required(),
+  author: Joi.object({
+    name: Joi.string().required(),
+    username: Joi.string().required().allow(null),
+    website: Joi.string().required().allow(null)
+  }),
+  module_name: Joi.string().required(),
+  publish_date: Joi.string().regex(/^\d{4}-\d{2}-\d{2}$/).required(),
+  vulnerable_versions: Joi.string().allow(null).required(),
+  patched_versions: Joi.string().allow(null).required(),
+  overview: Joi.string().required(),
+  recommendation: Joi.string().allow(null).required(),
+  references: Joi.array().allow(null).required(),
+  cvss_vector: Joi.string().allow(null).required(),
+  cvss_score: Joi.number().allow(null).required(),
+  coordinating_vendor: Joi.string().allow(null).required()
+});
+
 function validateVuln(filePath, model) {
   const vuln = JSON.parse(fs.readFileSync(filePath));
-  const result = coreModel.validate(vuln);
+  const result = model.validate(vuln);
   if (result.error) {
     console.error(filePath, result.error);
     throw result.error;
@@ -64,6 +86,7 @@ function validate(dir, model) {
 
 module.exports = {
   coreModel,
+  npmModel,
   validateVuln,
   validate
 };
